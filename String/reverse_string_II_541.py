b50f97d06ddfdaf6d9faf768a1d53ae5191156f8
hardenyu21 <hardenyu221@gmail.com>
2023-06-03 20:47:05 +0800

    Q541

--- String/reverse_string_II_541.py
+++ String/reverse_string_II_541.py
@@ -72,26 +72,3 @@ class Solution2(object):
             s = s[:p] + s[p: p2][::-1] + s[p2:]
             p = p + 2 * k
         return s
-
-
-class Solution3(object):
-    def reverseStr(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: str
-        """
-        def reverse_substring(text):
-            left, right = 0, len(text) - 1
-            while left < right:
-                text[left], text[right] = text[right], text[left]
-                left += 1
-                right -= 1
-            return text
-        
-        res = list(s)
-
-        for cur in range(0, len(s), 2 * k):
-            res[cur: cur + k] = reverse_substring(res[cur: cur + k])
-        
-        return ''.join(res)
